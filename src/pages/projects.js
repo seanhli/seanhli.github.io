4564775b00422bb728399c7ddec447a4af4bae49
Sean Li <sean.h.li.2017@gmail.com>
2023-02-05 19:39:26 -0800

    finalized projects descriptions, shipping v1 launch

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -19,8 +19,8 @@ function Projects() {
   }, [exiting]); // eslint-disable-line
 
   function exit(path) {
-    const projectsExit = projectsExitAnimation(projects ? projects.length : 0)
-    projectsExit.play()
+    const projectsExit = projectsExitAnimation(projects ? projects.length : 0);
+    projectsExit.play();
     setTimeout(() => {
       dispatch(toggleExiting());
       navigate(path);
@@ -32,19 +32,61 @@ function Projects() {
     "kanji life": [
       "https://seanhli.github.io/kanji-memo/",
       "https://github.com/seanhli/kanji-memo",
+      "JavaScript, CSS, React, Redux",
+      "sole developer and designer",
     ],
-    "gamify it!": [null, "https://gitlab.com/seanhli/gamify-it"],
+    "gamify it!": [
+      null,
+      "https://gitlab.com/seanhli/gamify-it",
+      "Python, JavaScript, CSS, FastAPI, PostgreSQL, React, Redux",
+      "sole developer and designer",
+    ],
     "soon-tm": [
       "https://ghost-busters.gitlab.io/soon-tm/",
       "https://gitlab.com/ghost-busters/soon-tm",
+      "Python, JavaScript, CSS, FastAPI, PostgreSQL, React, OAuth",
+      "developer in team of 4",
     ],
-    "wordle with cats": [null, "https://github.com/seanhli/wordle-with-cats"],
+    "wordle with cats": [
+      null,
+      "https://github.com/seanhli/wordle-with-cats",
+      "Python, Django, CSS",
+      "sole developer and designer",
+    ],
   };
   const projectContent = {
-    "kanji life": ["description",["1", "2"]],
-    "gamify it!": ["description",["1", "2"]],
-    "soon-tm": ["description",["1", "2"]],
-    "wordle with cats": ["description",["1", "2"]],
+    "kanji life": [
+      "an interactive flashcard & quiz web app for those interested in Japanese kanji/vocab",
+      [
+        "developed for former (myself!) and current students learning Japanese to have a quick, fun and interactive way of practicing",
+        "streamlined API calls and performance scaling through the use of redux and RTK query",
+        "implemented a fully functional, highly responsive drag-and-drop quiz interface",
+        "currently looking to optimize for mobile interfaces and integrate with ANKI to store vocabulary that users have trouble with",
+      ],
+    ],
+    "gamify it!": [
+      "(in dev) a web app for users to gamify their tasks and customize their rewards",
+      [
+        "inspired by my former life designing and managing rewards programs",
+        "designed to help users build habits around healthy goals that they set for themselves",
+        "utilized FastAPI for a RESTful API backend and PostgreSQL for managing app data",
+      ],
+    ],
+    "soon-tm": [
+      "a web service enabling users to connect with friends and loved ones through scheduled reminders",
+      [
+        "created for all of us that need an extra reminder to tell our loved ones and friends that we miss them",
+        "combined FastAPI and PostgreSQL for complete RESTful API backends across 4 different microservices",
+        "integrated with gmail to create a dedicated emailing microservice",
+      ],
+    ],
+    "wordle with cats": [
+      "an unlimited version of wordle with custom word length options",
+      [
+        "first major foray into building web apps with django",
+        "used Django MVT architecture to build a local, customizable version of wordle which tracks user wordle history by difficulty level",
+      ],
+    ],
   };
 
   return (
@@ -73,13 +115,19 @@ function Projects() {
                   rel="noopener noreferrer"
                 >
                   repo
-                </a>
+                </a>{" "}
+                | <span>{projectHeader[project][3]}</span>
               </div>
               <div className="project-card-content">
-                <div className="project-card-description">{projectContent[project][0]}</div>
+                <div className="project-card-tech">
+                  {projectHeader[project][2]}
+                </div>
+                <div className="project-card-description">
+                  {projectContent[project][0]}
+                </div>
                 {projectContent[project][1].map((item) => {
                   return (
-                    <li>
+                    <li key={`li-${item}`}>
                       <span className="li-text">{item}</span>
                     </li>
                   );
