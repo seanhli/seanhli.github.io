a876abf6bb69399f08748d63163ac9fbed993236
Sean Li <sean.h.li.2017@gmail.com>
2023-04-10 15:11:11 -0700

    added bg and bg hex animation

--- src/pro_ver/proBackground.js
+++ src/pro_ver/proBackground.js
@@ -1,7 +1,21 @@
 function ProBackground() {
-    return (
-        <div className="p-background"></div>
-    )
+  return (
+    <div className="p-background-container">
+      <div className="p-background"></div>
+      <div className="p-animated-hexes">
+        <div className="hex-1"></div>
+        <div className="hex-2"></div>
+        <div className="hex-1"></div>
+        <div className="hex-1"></div>
+        <div className="hex-1"></div>
+        <div className="hex-1"></div>
+        <div className="hex-2"></div>
+        <div className="hex-2"></div>
+        <div className="hex-2"></div>
+        <div className="hex-2"></div>
+      </div>
+    </div>
+  );
 }
 
 export default ProBackground;
